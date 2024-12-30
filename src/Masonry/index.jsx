f9ae67a7e5ed2a58cc546e394a67809aa08fc4e9
aedenmurray <aeden@aedenmurray.dev>
2024-12-29 19:55:51 -0700

    update

--- src/Masonry/index.jsx
+++ src/Masonry/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable import/no-extraneous-dependencies */
-import { useEffect, useState, Children } from 'react';
+import { Children, useMemo } from 'react';
 import { useColumns } from '../ColumnsContext';
 import Container from './Container';
 import Column from './Column';
@@ -12,33 +12,32 @@ function Masonry({ children, columns, gap = 8 }) {
   const columnsFromContext = useColumns();
   const columnsValue = columns ?? columnsFromContext;
 
-  const distribute = () => {
-    const childrenArray = Children.toArray(children);
-    const empty = createEmptyColumns(columnsValue);
+  const matrix = useMemo(
+    () => {
+      const childrenArray = Children.toArray(children);
+      const empty = createEmptyColumns(columnsValue);
 
-    return childrenArray
-      .reduce((acc, curr, idx) => {
-        const columnIdx = (idx % columnsValue);
-        acc[columnIdx].push(curr);
-        return acc;
-      }, empty);
-  };
-
-  const [matrix, setMatrix] = useState(distribute);
-
-  useEffect(
-    () => { setMatrix(distribute); },
-    [children, columnsValue],
+      return childrenArray
+        .reduce((acc, curr, idx) => {
+          const columnIdx = (idx % columnsValue);
+          acc[columnIdx].push(curr);
+          return acc;
+        }, empty);
+    },
+    [
+      columnsValue,
+      children,
+    ],
   );
 
   return (
     <Container gap={gap}>
       {matrix.map(
-        (column, idx) => (
+        (items, idx) => (
           <Column
             key={idx}
             gap={gap}
-            items={column}
+            items={items}
           />
         ),
       )}
